a62d6898ba97440d8471aecb1b67ae2e19965532
webzhang <zhanghuag@yonyou.com>
2019-11-27 20:59:16 +0800

    add(tools:extractTagsA提取字符串中a标签数据方法)

---
+++ tools.js
@@ -0,0 +1,21 @@
+/**
+ * 提取字符串中的a标签
+ * @param   {string}    [str]   str
+ * @readonly    {object}    [{source:'',url:'',text:''}]
+ */
+// var str='<p>需要到EHR员工自助\n\n<a href=\"http://www.baidu.com/hrss/login.jsp\">www.baidu.com</a>&nbsp; 个人信息中进行修改，修改完成后需要所在组织的HRBP审核后的一天时间（系统有一天的同步时间）才能生效。</p><p>建议使用IE浏览器登录，如果登录进去显示页面有问题，需要将登陆地址加入浏览器的兼容性视图；</p><p>如果登录还有问题，可以将需要修改的手机号码发送给您的HRBP在后台进行修改。</p><p><img class=\"big-pic\" src=\"http://www.baidu.com/qyic8c7o%2F19f22374-60d6-4f72-9ac4-1b31a62881ec_QQ%E6%88%AA%E5%9B%BE20180709083727.png\" title=\"QQ截图20180709083727.png\" alt=\"QQ截图20180709083727.png\"></p><a href=\"http://www.baic.com/hrss/login.jsp\">http://www.baic.com/hrss/login.jsp</a><p><br></p><p></p>'
+function extractTagsA (str) {
+    let regexpA = /\<a.*?\>(.+?)\<\/a\>/g
+    let regexpUrl = /href\=\"(.+?)\"\>(.+?)\<\/a>/g
+    let labelAs = str.match(regexpA) || []
+    let newLabel = labelAs.map((item) => {
+        let labelContent = regexpUrl.exec(item)
+        regexpUrl.lastIndex = 0
+        return {
+            source: item,
+            url: labelContent[1],
+            text: labelContent[2]
+        }
+    })
+    return newLabel
+}
